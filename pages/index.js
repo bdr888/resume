f197555603a5eb92568bbd9f7e9326cbe901c1e7
Brian Ridge <brian.ridge@spiredigital.com>
2021-02-17 09:58:12 -0700

    fix typo

--- pages/index.js
+++ pages/index.js
@@ -3,40 +3,30 @@
 
 // see this issue about the jsxRuntime classic thing: https://github.com/vercel/next.js/discussions/18440
 
-import { jsx } from 'theme-ui'
+import { jsx, Text } from 'theme-ui'
 import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout'
 
 export default function Home() {
   return (
     <Layout>
-      <div className={styles.grid}>
-        <a href="https://nextjs.org/docs" className={styles.card}>
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className={styles.card}>
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
+      <Text sx={{ p: 2 }}>
+        Hi! I'm using this project to display some of the patterns and
+        approaches I take to designing web apps, as well as deploying sites.
+      </Text>
+      <Text sx={{ p: 2 }}>
+        See the code on
         <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className={styles.card}
+          href="https://github.com/briandridge/resume"
+          target="_blank"
+          sx={{ px: 2 }}
         >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
+          Github
         </a>
-
-        <a
-          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
-      </div>
+      </Text>
+      <Text sx={{ p: 2 }}>
+        It is a work in progress (obviously). More to come soon!
+      </Text>
     </Layout>
   )
 }
