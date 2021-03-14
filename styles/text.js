17aa7c5c284b19e0d5d64634cbcbf084cdcbdb2d
Brian Ridge <brian.ridge@spiredigital.com>
2021-03-14 15:44:00 -0600

    theme and text style updates, add individual font style files just for organization

---
+++ styles/text.js
@@ -0,0 +1,90 @@
+export const text = {
+  heading: {
+    fontFamily: 'heading',
+    lineHeight: 'heading',
+    fontWeight: 'heading',
+  },
+  h1: {
+    fontSize: [6, 7],
+    fontFamily: 'heading',
+    fontWeight: 'light',
+  },
+  h2: {
+    fontSize: [5, 6],
+    fontFamily: 'heading',
+    fontWeight: 'light',
+  },
+  h3: {
+    fontSize: [4, 5],
+    fontFamily: 'heading',
+    fontWeight: 'light',
+  },
+  h4: {
+    variant: 'text.heading',
+    fontSize: 4,
+    fontWeight: 'light',
+  },
+  h5: {
+    variant: 'text.heading',
+    fontSize: 4,
+    fontWeight: 'normal',
+  },
+  h6: {
+    variant: 'text.heading',
+    fontSize: 4,
+    fontWeight: 'normal',
+  },
+  largeText: {
+    fontFamily: 'body',
+    fontSize: 4,
+    fontWeight: 'normal',
+  },
+  largeTextBold: {
+    fontFamily: 'body',
+    fontSize: 4,
+    fontWeight: 'semiBold',
+  },
+  mediumText: {
+    fontFamily: 'body',
+    fontSize: 3,
+    fontWeight: 'normal',
+  },
+  mediumTextBold: {
+    fontFamily: 'body',
+    fontSize: 3,
+    fontWeight: 'semiBold',
+  },
+  smallText: {
+    fontFamily: 'body',
+    fontSize: 2,
+    fontWeight: 'normal',
+  },
+  smallTextBold: {
+    fontFamily: 'body',
+    fontSize: 2,
+    fontWeight: 'semiBold',
+  },
+  xSmallText: {
+    fontFamily: 'body',
+    fontSize: 1,
+    fontWeight: 'normal',
+  },
+  xxSmallText: {
+    fontFamily: 'body',
+    fontSize: 0,
+    fontWeight: 'normal',
+  },
+  allCaps: {
+    fontFamily: 'body',
+    fontSize: 1,
+    fontWeight: 'medium',
+    textTransform: 'uppercase',
+    letterSpacing: '1.07692px',
+  },
+  allCapsSmall: {
+    fontFamily: 'body',
+    fontSize: 0,
+    fontWeight: 'semiBold',
+    textTransform: 'uppercase',
+  },
+}
