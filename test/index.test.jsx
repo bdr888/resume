c41417844ba3c9c13a6f269bdd30a4400cd32aaf
briandridge <briandridge@gmail.com>
2021-03-31 13:03:34 -0600

    fix import

--- test/index.test.jsx
+++ test/index.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '../../test/test-utils'
+import { render, screen } from './test-utils'
 import Home, { GET_INTODUCTION } from '@pages/index'
 import { MockedProvider } from '@apollo/client/testing'
 
