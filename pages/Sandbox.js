36b31953dd5a49a308328f48a08ec6b881c9cef5
briandridge <briandridge@gmail.com>
2021-05-28 10:53:39 -0600

    updates sandbox page with props for heading

--- pages/Sandbox.js
+++ pages/Sandbox.js
@@ -1,28 +1,14 @@
 /** @jsxRuntime classic /
 /* @jsx jsx */
-import { jsx } from 'theme-ui'
+import { jsx, Flex } from 'theme-ui'
 import Layout from '../components/Layout'
 
-// const arrayWithDuplicates = [1, 1, 2, 2, 3, 3, 4]
-
-// const removeDuplicatesFromArray = arr =>
-//   arr.filter((item, index) => arr.indexOf(item) === index)
-
-// const dedupedresult = removeDuplicatesFromArray(arrayWithDuplicates)
-
-const unbalancedParens = '{[()]}}'
-const balancedParens = '{[()]}'
-
-// const determineBalanced = (string) => (
-//   unbalancedParens.
- 
-// )
-
-// const balancedresult = determineBalanced(balancedParens)
-// const unbalancedresult = determineBalanced(unbalancedParens)
-
 const Sandbox = () => {
-  return <Layout>Example Page</Layout>
+  return (
+    <Layout pageHeading="Sandbox" title="Sandbox">
+      <Flex>hello</Flex>
+    </Layout>
+  )
 }
 
-export default Sandbox 
+export default Sandbox
